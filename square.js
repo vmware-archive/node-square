4204639405501c56d77f66646fc9633377cfa29b
sxmon <sfbrey@gmail.com>
2018-11-08 23:23:08 +0100

    Update square.js

--- square.js
+++ square.js
@@ -1,4 +1,10 @@
+function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+}
+
 module.exports = x => {
+    sleepFor(2000);
     const xx = x ** 2;
     console.log(`the square of ${x} is ${xx}`);
     return xx;
